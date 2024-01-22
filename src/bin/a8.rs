74093cbe3a4df8e33107a917f8933a75150c5c28
Jinho Jang <verycosy@kakao.com>
2024-01-22 21:58:55 +0900

    a8

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,38 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavor {
+    Lemon,
+    Orange
+}
+
+struct Drink {
+    flavor: Flavor,
+    ounce: f64
+}
+
+fn print_drink(drink: Drink) {
+    print!("flavor: ");
+
+    match drink.flavor {
+        Flavor::Lemon => println!("lemon"),
+        Flavor::Orange => println!("orange")
+    }
+
+    println!("ounce: {:?}", drink.ounce);
+}
+
+fn main() {
+    let drink1 = Drink {
+        flavor: Flavor::Lemon,
+        ounce: 3.2
+    };
+
+    let drink2 = Drink {
+        flavor: Flavor::Orange,
+        ounce: 2.1
+    };
+
+    print_drink(drink1);
+    print_drink(drink2);
+}
