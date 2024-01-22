6d4361a44049440c953411a3afc774c2a6ceb6a2
Jinho Jang <verycosy@kakao.com>
2024-01-22 12:05:21 +0900

    a1

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,17 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+
+// * Use a function to display your first name
+fn print_first_name() {
+    println!("JinHo");
+}
+// * Use a function to display your last name
+fn print_last_name() {
+    println!("Jang");
+}
+
+fn main() {
+    print_first_name();
+    print_last_name();
+}
