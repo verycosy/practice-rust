e1ac92e5aa839ce9a60962767df226413972c8de
Jinho Jang <verycosy@kakao.com>
2024-01-22 12:15:51 +0900

    a2

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,19 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+// * Use a function to add two numbers together
+fn add(num1: i32, num2: i32) -> i32 {
+    num1 + num2
+}
+// * Use a function to display the result
+// * Use the "{:?}" token in the println macro to display the result
+fn print_number(num: i32) {
+    println!("{:?}", num);
+}
+
+fn main() {
+    let num1 = 3;
+    let num2 = 4;
+
+    print_number(add(num1, num2));
+}
