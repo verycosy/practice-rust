013a0920a2e39502031018fa9558e6cb16c1d131
Jinho Jang <verycosy@kakao.com>
2024-01-22 22:10:15 +0900

    a9

--- src/bin/a9.rs
+++ src/bin/a9.rs
@@ -9,4 +9,18 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn get_coordinates() -> (f64, f64)  {
+    (3.2, 5.4)
+}
+
+fn main() {
+    let (_, y) = get_coordinates();
+
+    if y > 5.0 {
+        println!("y > 5");
+    } else if y == 5.0 {
+        println!("y = 5");  
+    } else {
+        println!("y < 5");
+    }
+}
