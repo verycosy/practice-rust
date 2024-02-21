3acfacd19dc24cdddddc73bba52f6be9c676c6de
Jinho Jang <verycosy@kakao.com>
2024-02-21 09:47:47 +0900

    a11

--- src/bin/a11.rs
+++ src/bin/a11.rs
@@ -9,4 +9,25 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct Grocery {
+	quantity: i32,
+	id: i32
+}
+
+fn display_quantity(grocery: &Grocery) {
+	println!("quantity : {:?}", grocery.quantity)
+}
+
+fn display_id(grocery: &Grocery) {
+	println!("id : {:?}", grocery.id)
+}
+
+fn main() {
+	let grocery = Grocery {
+		id: 1,
+		quantity: 2
+	};
+
+	display_quantity(&grocery);
+	display_id(&grocery);
+}
