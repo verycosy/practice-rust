8aad362f0e540cc0d3c5e51e8cc2e84cefb67d2b
Jinho Jang <verycosy@kakao.com>
2024-03-14 10:15:32 +0900

    a13

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,4 +10,22 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    let numbers = vec![10,20,30,40]; // 메인 함수 소유
+
+    // for num in numbers { // for문 소유로 넘어갔다가 반복이 끝나면 사용불가
+    for num in &numbers {
+        if *num == 30 {
+            println!("thirty");
+        } else {
+            println!("{:?}", num);
+        }
+
+        // match num {
+        //     30 => println!("thirty"),
+        //     _ => println!("{:?}", num)
+        // }
+    }
+
+    println!("length : {:?}", numbers.len());
+}
