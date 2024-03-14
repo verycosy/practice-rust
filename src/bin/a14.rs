7ec83c9b27554a33f499294131853677a91b6336
Jinho Jang <verycosy@kakao.com>
2024-03-14 10:27:42 +0900

    a14

--- src/bin/a14.rs
+++ src/bin/a14.rs
@@ -11,4 +11,43 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    color: String
+}
+
+fn print_info(name: &str, color: &str) {
+    println!("Name: {:?}, Color: {:?}", name, color);
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            age: 12,
+            name: String::from("Alice"),
+            color: String::from("Red")
+        },
+        Person {
+            age: 10,
+            name: String::from("Bob"),
+            color: String::from("Blue")
+        },
+        Person {
+            age: 2,
+            name: String::from("Chris"),
+            color: String::from("Green")
+        },
+        Person {
+            age: 14,
+            name: String::from("David"),
+            color: String::from("White")
+        },
+    ];
+
+    for person in people {
+        if person.age <= 10 {
+            print_info(&person.name, &person.color);
+        }
+    }
+}
